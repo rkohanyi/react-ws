ca6f6f045d91e44e86058c8b4c8b37415b72713a
Kohányi Róbert <kohanyi.robert@gmail.com>
2026-02-05 11:58:48 +0100

    Fix clearning inputs

--- src/components/AddNewMovie.jsx
+++ src/components/AddNewMovie.jsx
@@ -3,16 +3,22 @@ import { useState } from "react"
 export function AddNewMovie({ onMovieAdd }) {
     console.log("AddNewMovie instantiated")
 
-    const [id, setId] = useState(null)
-    const [title, setTitle] = useState(null)
+    const [id, setId] = useState("")
+    const [title, setTitle] = useState("")
+
+    function handleMovieAdd() {
+        onMovieAdd(id, title)
+        setId("")
+        setTitle("")
+    }
 
     return <div>
         <h2>Add New Movie</h2>
 
         <label>ID</label>
-        <input onInput={(event) => setId(event.target.value)}></input>
+        <input value={id} onInput={(event) => setId(event.target.value)}></input>
         <label>Title</label>
-        <input onInput={(event) => setTitle(event.target.value)}></input>
-        <button onClick={() => onMovieAdd(id, title)}>Add new movie!</button>
+        <input value={title} onInput={(event) => setTitle(event.target.value)}></input>
+        <button onClick={() => handleMovieAdd()}>Add new movie!</button>
     </div>
 }
