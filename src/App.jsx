1ad665f6976605f7f6f0237ef569657db516a46f
Kohányi Róbert <kohanyi.robert@gmail.com>
2026-02-05 11:52:44 +0100

    Add delete movie functionality & fetching from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,61 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { MovieCard } from './components/MovieCard'
 import { AddNewMovie } from './components/AddNewMovie'
 
+function mapMovie(movie) {
+  return {
+    id: movie.id,
+    title: movie.original_title,
+  }
+}
+
 function App() { // Top movie component thingy
   console.log('App instantiated')
-
-  const [movieList, setMovieList] = useState([
-    {
-      id: 1,
-      title: 'Die Hard 1'
-    },
-    {
-      id: 2,
-      title: 'Die Hard 2'
-    },
-    {
-      id: 3,
-      title: 'Die Hard 3'
+  
+  const [movieList, setMovieList] = useState([])
+  const [lastSelectedId, setLastSelectedId] = useState(-1)
+  
+  useEffect(() => {
+    async function fetchMovies() {
+      const res = await fetch('https://jsonfakery.com/movies/random/3')
+      setMovieList((await res.json()).map(mapMovie))
     }
-  ])
+    fetchMovies()
+  }, [])
 
-  const [lastSelectedIndex, setLastSelectedIndex] = useState(-1)
-
-  function updateLastSelected(index) {
-    setLastSelectedIndex(index)
+  function updateLastSelected(id) {
+    setLastSelectedId(id)
   }
 
   function updateMovieList(id, title) {
     console.log("New movie added: ", id, title)
-    const newMovie = { id, title };
-    setMovieList([...movieList, newMovie])
+    const newMovie = { id, title }
+    const newMovieList = [...movieList]
+    newMovieList.push(newMovie)
+    setMovieList(newMovieList)
+  }
+
+  function deleteSelected(id) {
+    setMovieList(movieList.filter(movie => movie.id !== id))
   }
 
   return (
     <>
       <h1>Top Level Movie Thingy</h1>
       {
-        lastSelectedIndex === -1
+        lastSelectedId === -1
           ? <h3>None selected yet</h3>
-          : <h3>{movieList[lastSelectedIndex].title}</h3>
+          : <h3>{movieList[lastSelectedId].title}</h3>
       }
       <hr></hr>
-      <AddNewMovie onMovieAdd={updateMovieList}/>
+      <AddNewMovie onMovieAdd={updateMovieList} />
       <hr></hr>
       {
-        movieList.map((movie, index) => <MovieCard key={movie.id}
-          index={index}
+        movieList.map(movie => <MovieCard key={movie.id}
           id={movie.id}
           title={movie.title}
-          onSelected={updateLastSelected} />)
+          onSelected={updateLastSelected}
+          onDeleted={deleteSelected} />)
       }
     </>)
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,10 @@
-export function MovieCard({ index, id, title, onSelected }) {
+export function MovieCard({ id, title, onSelected, onDeleted }) {
     console.log("MovieCard instantiated")
 
     return <div id={"movie-" + id}>
         <h1>Hey I'm Mr. Movie Card!</h1>
         <h2>{title}</h2>
-        <button onClick={() => onSelected(index)}>Click me!</button>
+        <button onClick={() => onSelected(id)}>Click me!</button>
+        <button onClick={() => onDeleted(id)}>Delete me!</button>
     </div>
 }
