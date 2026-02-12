4242e4a773a0864a8d35aa9c1fd186f1ab566b96
Kohányi Róbert <kohanyi.robert@gmail.com>
2026-02-12 09:20:45 +0100

    Link label with input in component

--- src/components/AddNewMovie.jsx
+++ src/components/AddNewMovie.jsx
@@ -15,10 +15,10 @@ export function AddNewMovie({ onMovieAdd }) {
     return <div>
         <h2>Add New Movie</h2>
 
-        <label>ID</label>
-        <input value={id} onInput={(event) => setId(event.target.value)}></input>
-        <label>Title</label>
-        <input value={title} onInput={(event) => setTitle(event.target.value)}></input>
+        <label htmlFor="movie-id">ID</label>
+        <input id="movie-id" value={id} onInput={(event) => setId(event.target.value)}></input>
+        <label htmlFor="movie-title">Title</label>
+        <input id="movie-title" value={title} onInput={(event) => setTitle(event.target.value)}></input>
         <button onClick={() => handleMovieAdd()}>Add new movie!</button>
     </div>
 }
